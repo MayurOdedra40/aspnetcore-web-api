dee29ad31070f0387b5f223252188f1b6be5d12b
Mayur Odedra <mayurodedra.40st@gmail.com>
2022-06-10 16:25:49 +0530

    added dbcontext and model and book controller

---
+++ WebAPICore/Controllers/BooksController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPICore.Data.Services;
+using WebAPICore.Data.ViewModels;
+
+namespace WebAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody]BookVM book) 
+        {
+            _bookService.AddBook(book);
+            return Ok();
+        }
+    }
+}
--- WebAPICore/Startup.cs
+++ WebAPICore/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,14 +12,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPICore.Data;
+using WebAPICore.Data.Services;
 
 namespace WebAPICore
 {
     public class Startup
     {
+        public string ConnectionString { get; set; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ConnectionString = Configuration.GetConnectionString("DefaultConnectionString");
         }
 
         public IConfiguration Configuration { get; }
@@ -28,6 +34,13 @@ namespace WebAPICore
         {
 
             services.AddControllers();
+
+            //configure dbcontext with sql
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(ConnectionString));
+
+            //congfigure the services
+            services.AddTransient<BookService>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPICore", Version = "v1" });
@@ -54,6 +67,8 @@ namespace WebAPICore
             {
                 endpoints.MapControllers();
             });
+
+            AppDbInitializer.Seed(app);
         }
     }
 }
