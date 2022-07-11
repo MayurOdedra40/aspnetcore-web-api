8d089a4203a9afbecfcf6e107217c854ba6066fa
Mayur Odedra <mayurodedra.40st@gmail.com>
2022-07-11 17:13:16 +0530

    CRUD complete

--- WebAPICore/Controllers/BooksController.cs
+++ WebAPICore/Controllers/BooksController.cs
@@ -20,10 +20,38 @@ namespace WebAPICore.Controllers
             _bookService = bookService;
         }
 
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _bookService.GetAllBooks();
+            return Ok(allBooks);
+        }
+
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _bookService.GetBookById(id);
+            return Ok(book);
+        }
+
         [HttpPost("add-book")]
         public IActionResult AddBook([FromBody]BookVM book) 
         {
             _bookService.AddBook(book);
+            return Ok();
+        }
+
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody]BookVM book)
+        {
+            var updatedBook = _bookService.UpdateBookById(id, book);
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("delete-book-by-id/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+            _bookService.DeleteBookById(id);
             return Ok();
         }
     }
